10248e4572b1e7a6db471a6b7308ca49ed2b48b0
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-11-04 17:20:24 -0800

    create intake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Intake.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+    DcMotor flywheel;
+    enum IntakeDirection {IN, OUT, STOP};
+    IntakeDirection currentIntakeState = IntakeDirection.IN;
+    public void init(HardwareMap hm) {
+        flywheel = hm.get(DcMotor.class, "flywheel");
+    }
+    public void Loop(Gamepad gp1, Gamepad gp2) {
+        if (gp1.a) {
+            currentIntakeState = IntakeDirection.IN;
+        } else if (gp1.b){
+            currentIntakeState = IntakeDirection.OUT;
+        } else {
+            currentIntakeState = IntakeDirection.STOP;
+        }
+        switch (currentIntakeState) {
+            case IN:
+                flywheel.setPower(0.8);
+                break;
+            case OUT:
+                flywheel.setPower(-0.8);
+                break;
+            case STOP:
+                flywheel.setPower(0);
+                break;
+        }
+    }
+}
