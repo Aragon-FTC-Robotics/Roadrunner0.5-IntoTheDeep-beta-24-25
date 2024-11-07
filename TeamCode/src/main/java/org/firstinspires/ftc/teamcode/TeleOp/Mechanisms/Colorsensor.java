ebe148447da0c611995323da191171fe0e7e723e
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-11-07 10:53:59 -0800

    add color check method to colorsensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Colorsensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Colorsensor.java
@@ -10,6 +10,12 @@ public class Colorsensor {
     NormalizedColorSensor colorSensor;
     NormalizedRGBA color;
     float gain = (float) 2.5;
+    public float[] redHigher = {255,50,50}; //CLOSER TO WHITE
+    public float[] redLower = {100,0,0}; //CLOSER TO BLACK
+    public float[] yellowHigher = {255,255,50}; //etc
+    public float[] yellowLower = {100,100,0};
+    public float[] blueHigher = {50,50,255};
+    public float[] blueLower = {0,0,100};
     public void init(HardwareMap hm) {
         colorSensor = hm.get(NormalizedColorSensor.class, "sensor_color");
         colorSensor.setGain(gain);
@@ -25,6 +31,18 @@ public class Colorsensor {
         return new float[] {color.red,color.green,color.blue};
     }
     public boolean sensorIsRed() {
-        return color.red>100&&color.green<50&&color.blue<50;
+        return colorInRange(new float[] {color.red,color.green,color.blue},redLower,redHigher);
+    }
+    public boolean sensorIsYellow() {
+        return colorInRange(new float[] {color.red,color.green,color.blue},yellowLower,yellowHigher);
+    }
+    public boolean sensorIsBlue() {
+        return colorInRange(new float[] {color.red,color.green,color.blue},blueLower,blueHigher);
+    }
+    public boolean colorInRange(float[] color, float[] min, float[] max) {
+        return
+                min[0] < color[0] && color[0] < max[0] && //Red is within min and max range
+                min[1] < color[1] && color[1] < max[1] && //Green is within min and max range
+                min[2] < color[2] && color[2] < max[2];   //brue is sithin the range,
     }
 }
