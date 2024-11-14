83c7cb958b234aa548e3646ce9110c9ffb06a77d
InfernoFox06 <infernofox06@gmail.com>
2024-11-13 16:17:32 -0800

    Updated Bar
    Fixed bar to have states, added 2nd servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Bar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Bar.java
@@ -8,7 +8,9 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 public class Bar {
-    Servo barServo;
+    Servo barServoLeft;
+    Servo barServoRight;
+
     enum BarState {TRANSFER, WALL, BUCKET, CLIPUP, CLIPDOWN}
     double pos;
     double transferpos = 0;
@@ -18,25 +20,54 @@ public class Bar {
     double clipdownpos = 0;
 
     BarState currentBarState;
+
     public void init(HardwareMap hm) {
-        barServo = hm.get(Servo.class, "bar");
-//        barServo.setDirection(Servo.Direction.REVERSE);
+        barServoLeft = hm.get(Servo.class, "barLeft");
+        barServoRight = hm.get(Servo.class, "barRight");
+        barServoRight.setDirection(Servo.Direction.REVERSE);
+        currentBarState = BarState.TRANSFER;
     }
     public void Loop(Gamepad gp1, Gamepad gp2) {
         if (gp1.a) { //ALL OF THESE ARE PLACEHOLDERS type baet
-            pos = transferpos;
+            setState(BarState.TRANSFER);
         } else if (gp2.dpad_up || gp2.dpad_right) {
-            pos = bucketpos;
+            setState(BarState.BUCKET);
         } else if (gp2.b) {
-            pos = wallpos;
+            setState(BarState.WALL);
         } else if (gp2.a) {
-            pos = clipuppos;
+            setState(BarState.CLIPUP);
         } else if (gp2.y) {
-            pos = clipdownpos;
-        } else if (gp2.x) {
-            pos = transferpos;
+            setState(BarState.CLIPDOWN);
         }
-        barServo.setPosition(pos);
+
+        switch(currentBarState) {
+            case TRANSFER:
+                setPos(transferpos);
+                break;
+            case BUCKET:
+                setPos(bucketpos);
+                break;
+            case WALL:
+                setPos(wallpos);
+                break;
+            case CLIPUP:
+                setPos(clipuppos);
+            case CLIPDOWN:
+                setPos(clipdownpos);
+                break;
+            default:
+                setState(BarState.TRANSFER);
+                break;
+        }
     }
+    public void setPos(double pos) {
+        barServoLeft.setPosition(pos);
+        barServoRight.setPosition(pos);
+    }
+
+    public void setState(BarState bar) {
+        currentBarState = bar;
+    }
+
     public double getPos() {return pos;}
 }
