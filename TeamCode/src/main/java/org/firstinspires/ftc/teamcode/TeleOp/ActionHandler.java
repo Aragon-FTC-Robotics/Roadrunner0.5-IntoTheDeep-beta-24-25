449c0e743660528d613a9dff6978998d1e5ef54a
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-12-04 21:01:27 -0800

    tune kv and rewrite actionhandler

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ActionHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ActionHandler.java
@@ -1,20 +1,10 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-import android.graphics.Color;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.*;
 
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Bar;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Colorsensor;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Drivetrain;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Extendo;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Flywheel;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.IntakeWrist;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Slides;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Wrist;
-import org.firstinspires.ftc.teamcode.TeleOp.Mechanisms.Claw;
-
+import java.util.concurrent.TimeUnit;
 
 public class ActionHandler {
     private Slides slides;
@@ -25,123 +15,173 @@ public class ActionHandler {
     private Claw claw;
     private IntakeWrist intakeWrist;
     private Colorsensor colorSensor;
-    private boolean intook = false;
     private boolean intaking = false;
     private String alliance;
-    public void init(Slides s, Extendo e, Bar b, Wrist w, Flywheel f, Claw c, IntakeWrist iw, Colorsensor cs, String alliance){
-        slides=s;extendo=e;bar=b;wrist=w;flywheel=f;claw=c;intakeWrist=iw;colorSensor=cs;
+
+    private ElapsedTime timer = new ElapsedTime();
+    private ActionState currentActionState = ActionState.IDLE;
+
+    enum ActionState {
+        IDLE,
+        TRANSFER_STAGE_1,
+        TRANSFER_STAGE_2,
+        TRANSFER_STAGE_3,
+        CLIP2_STAGE_1,
+        CLIP2_STAGE_2
+    }
+
+    public void init(Slides s, Extendo e, Bar b, Wrist w, Flywheel f, Claw c, IntakeWrist iw, Colorsensor cs, String alliance) {
+        slides = s;
+        extendo = e;
+        bar = b;
+        wrist = w;
+        flywheel = f;
+        claw = c;
+        intakeWrist = iw;
+        colorSensor = cs;
         this.alliance = alliance;
     }
-    public void Loop(Gamepad gp1, Gamepad gp2) throws InterruptedException {
-        //Intake action, runs intake + color sensor check
+
+    public void Loop(Gamepad gp1, Gamepad gp2) {
+        handleTimedActions();
+
+        // Intake action
         if (gp1.y && !intaking) {
             intaking = true;
             flywheel.setState(Flywheel.FlywheelDirection.IN);
         }
         intakecheck();
-        //LATER: MAKE IT RUMBLE OR SOMETHING
-        if(gp1.left_bumper) {
-            Transfer();
+
+        // Initiate transfer
+        if (gp1.left_bumper) {
+            startTransfer();
         }
-        if(gp1.right_bumper) {
-            Thread.sleep(500);
+
+        // Intake wrist out
+        if (gp1.right_bumper) {
             intakeWrist.setState(IntakeWrist.iwristState.OUT);
         }
-        //Bucket high
-        if(gp2.dpad_up) {
+
+        // Bucket high
+        if (gp2.dpad_up) {
             highBucket();
         }
-        if(gp2.dpad_down) {
+
+        // Reset slides and arm
+        if (gp2.dpad_down) {
             resetSlidesArm();
         }
 
-        //clip Up
-        if(gp2.b) {
+        // Clip up
+        if (gp2.b) {
             Clip1();
         }
 
-        //Clip down
-        if(gp2.y) {
-            Clip2();
+        // Clip down
+        if (gp2.y) {
+            startClip2();
         }
-        //Wall pickup
-        if(gp2.a) {
+
+        // Wall pickup
+        if (gp2.a) {
             claw.setState(Claw.ClawState.OPEN);
             bar.setState(Bar.BarState.WALL);
             wrist.setState(Wrist.wristState.WALL);
         }
-        //transfer
-        if(gp1.b) {
-            Transfer();
+
+        // Transfer
+        if (gp1.b) {
+            startTransfer();
         }
-        //intakewristdown
-        if(gp1.x) {
+
+        // Intake wrist down
+        if (gp1.x) {
             intakeWrist.setState(IntakeWrist.iwristState.OUT);
         }
     }
 
-    public void Transfer() throws InterruptedException {
-        claw.setState(Claw.ClawState.CLOSE);
-        Thread.sleep(500);
-        intakeWrist.setState(IntakeWrist.iwristState.IN);
-        Thread.sleep(500);
-        flywheel.setState(Flywheel.FlywheelDirection.OUT);
-        Thread.sleep(250);
-        flywheel.setState(Flywheel.FlywheelDirection.STOP);
-        bar.setState(Bar.BarState.TRANSFER);
-        wrist.setState(Wrist.wristState.TRANSFER);
-        Thread.sleep(2000);
-        claw.setState(Claw.ClawState.OPEN);
-        Thread.sleep(200);
-        bar.setState(Bar.BarState.WALL);
+    private void handleTimedActions() {
+        long elapsedMs = timer.time(TimeUnit.MILLISECONDS);
+
+        switch (currentActionState) {
+            case TRANSFER_STAGE_1:
+                if (elapsedMs >= 500) {
+                    intakeWrist.setState(IntakeWrist.iwristState.IN);
+                    currentActionState = ActionState.TRANSFER_STAGE_2;
+                    timer.reset();
+                }
+                break;
+            case TRANSFER_STAGE_2:
+                if (elapsedMs >= 500) {
+                    flywheel.setState(Flywheel.FlywheelDirection.OUT);
+                    currentActionState = ActionState.TRANSFER_STAGE_3;
+                    timer.reset();
+                }
+                break;
+            case TRANSFER_STAGE_3:
+                if (elapsedMs >= 250) {
+                    flywheel.setState(Flywheel.FlywheelDirection.STOP);
+                    bar.setState(Bar.BarState.TRANSFER);
+                    wrist.setState(Wrist.wristState.TRANSFER);
+                    claw.setState(Claw.ClawState.OPEN);
+                    currentActionState = ActionState.IDLE;
+                }
+                break;
+            case CLIP2_STAGE_1:
+                if (elapsedMs >= 200) {
+                    claw.setState(Claw.ClawState.OPEN);
+                    currentActionState = ActionState.IDLE;
+                }
+                break;
+            default:
+                break;
+        }
     }
+
+    private void startTransfer() {
+        claw.setState(Claw.ClawState.CLOSE);
+        currentActionState = ActionState.TRANSFER_STAGE_1;
+        timer.reset();
+    }
+
+    private void startClip2() {
+        bar.setState(Bar.BarState.CLIP2);
+        wrist.setState(Wrist.wristState.CLIP2);
+        currentActionState = ActionState.CLIP2_STAGE_1;
+        timer.reset();
+    }
+
     public void Clip1() {
         bar.setState(Bar.BarState.CLIP1);
         wrist.setState(Wrist.wristState.CLIP1);
     }
-    public void Clip2() throws InterruptedException{
-        bar.setState(Bar.BarState.CLIP2);
-        wrist.setState(Wrist.wristState.CLIP2);
-        Thread.sleep(200);
-        claw.setState(Claw.ClawState.OPEN);
-    }
-    public void intakecheck() throws InterruptedException{
-        if (intaking) {
 
-            if (
-                    ((alliance == "red") && (colorSensor.sensorIsRed() || colorSensor.sensorIsYellow()))
-                            ||  ((alliance == "blue") && (colorSensor.sensorIsBlue() || colorSensor.sensorIsYellow()))
-            ) { //Intakes correct color
-                Thread.sleep(200);
-                if (
-                        ((alliance == "red") && (colorSensor.sensorIsRed() || colorSensor.sensorIsYellow()))
-                                ||  ((alliance == "blue") && (colorSensor.sensorIsBlue() || colorSensor.sensorIsYellow()))
-                ) {
-                    flywheel.setState(Flywheel.FlywheelDirection.STOP);
-                    intaking = false;
-                }
-            }
-            if (
-                    ((alliance == "red") && colorSensor.sensorIsBlue())
-                            ||  ((alliance == "blue") && colorSensor.sensorIsRed())
-            ) {
-                Thread.sleep(200);
-                if (
-                        ((alliance == "red") && colorSensor.sensorIsBlue())
-                        ||  ((alliance == "blue") && colorSensor.sensorIsRed())
-                ) {
+    public void intakecheck() {
+        if (intaking) {
+            boolean correctColor = (alliance.equals("red") && (colorSensor.sensorIsRed() || colorSensor.sensorIsYellow()))
+                    || (alliance.equals("blue") && (colorSensor.sensorIsBlue() || colorSensor.sensorIsYellow()));
+
+            if (correctColor) {
+                flywheel.setState(Flywheel.FlywheelDirection.STOP);
+                intaking = false;
+            } else {
+                boolean wrongColor = (alliance.equals("red") && colorSensor.sensorIsBlue())
+                        || (alliance.equals("blue") && colorSensor.sensorIsRed());
+
+                if (wrongColor) {
                     flywheel.setState(Flywheel.FlywheelDirection.OUT);
-                    Thread.sleep(1000);
-                    flywheel.setState(Flywheel.FlywheelDirection.IN);
+                    intaking = false; // Stops after correcting
                 }
             }
         }
     }
+
     public void highBucket() {
         slides.setState(Slides.slideState.HIGH);
         bar.setState(Bar.BarState.BUCKET);
         wrist.setState(Wrist.wristState.BUCKET);
     }
+
     public void resetSlidesArm() {
         slides.setState(Slides.slideState.GROUND);
         bar.setState(Bar.BarState.WALL);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0;
+    public static double kV = 0.03333;
     public static double kA = 0;
     public static double kStatic = 0;
 
