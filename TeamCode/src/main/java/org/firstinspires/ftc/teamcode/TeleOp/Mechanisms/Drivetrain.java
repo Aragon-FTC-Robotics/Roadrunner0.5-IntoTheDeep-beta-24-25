e87c6adf279b44a825ae9bb3639fb1f8c93b7438
twenty-one21 <31107399+twenty-one21@users.noreply.github.com>
2024-12-05 19:16:10 -0800

    add gp1 strafing buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanisms/Drivetrain.java
@@ -36,25 +36,30 @@ public class Drivetrain {
 
     public void Loop(Gamepad gp1, Gamepad gp2){
 
-
+        double MICROSPEED = 0.1;
         double y = gp1.left_stick_y; // Remember, Y stick value is reversed
         double x = -gp1.left_stick_x * 1.1; // Counteract imperfect strafing
         double rx = -gp1.right_stick_x * 0.8;
 
+        if (gp1.dpad_up) {
+            x = 0; y = -MICROSPEED;
+        }
+        if (gp1.dpad_down) {
+            x = 0; y = MICROSPEED;
+        }
+        if (gp1.dpad_right) {
+            x = MICROSPEED; y = 0;
+        }
+        if (gp1.dpad_left) {
+            x = -MICROSPEED; y = 0;
+        }
         double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
 
-        if(slowMode == false) {
-            minSpeed = -0.6;
-            maxSpeed = 0.6;
 
-        } else if(slowMode == true) {
-            minSpeed = -0.3;
-            maxSpeed = 0.3;
-        }
 
 
         rightFront.setPower(Range.clip(frontRightPower, minSpeed, maxSpeed));
